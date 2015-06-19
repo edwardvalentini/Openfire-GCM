e228eb06cdd2d03f33e535f6efe25f8c7a9b0525
gologuzov <gologuzov@bifit.com>
2015-06-20 01:52:53 +0300

    Fixed compilation errors

--- src/java/de/meisterfuu/openfire/gcm/GcmPlugin.java
+++ src/java/de/meisterfuu/openfire/gcm/GcmPlugin.java
@@ -1,14 +1,7 @@
 package de.meisterfuu.openfire.gcm;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
-import java.util.TimerTask;
-
+import com.google.gson.Gson;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.fluent.Form;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.entity.ContentType;
 import org.jivesoftware.openfire.PresenceManager;
 import org.jivesoftware.openfire.XMPPServer;
 import org.jivesoftware.openfire.container.Plugin;
@@ -21,8 +14,9 @@ import org.jivesoftware.openfire.user.User;
 import org.jivesoftware.openfire.user.UserManager;
 import org.jivesoftware.openfire.user.UserNameManager;
 import org.jivesoftware.openfire.user.UserNotFoundException;
+import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.util.JiveGlobals;
-import org.jivesoftware.util.PropertyEventListener;
+import org.jivesoftware.util.ParamUtils;
 import org.jivesoftware.util.TaskEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,222 +25,261 @@ import org.xmpp.packet.Message;
 import org.xmpp.packet.Packet;
 import org.xmpp.packet.Presence;
 
-import com.google.gson.Gson;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimerTask;
 
 public class GcmPlugin implements Plugin, PacketInterceptor {
 
-	private static final Logger Log = LoggerFactory.getLogger(GcmPlugin.class);
-	private static final String URL = "plugin.gcmh.url";
-	private static final String MODE = "plugin.gcmh.mode";
-	private static final String DEBUG = "plugin.gcmh.debug";
+    private static final Logger Log = LoggerFactory.getLogger(GcmPlugin.class);
+    private static final String GCM_HOST = "plugin.gcmh.host";
+    private static final String GCM_PORT = "plugin.gcmh.port";
+    private static final String GCM_API_KEY = "plugin.gcmh.api_key";
+    private static final String GCM_SENDER_ID = "plugin.gcmh.sender_id";
 
-	public static final String MODE_ALL = "1";
-	public static final String MODE_OFFLINE = "2";
-	public static final String MODE_NO_MOBILE = "3";
-	public static final String MODE_EXCEPTION = "4";
-
-	public static final String DEBUG_ON = "1";
-	public static final String DEBUG_OFF = "2";
-	
-
-	public GcmPlugin() {
-		interceptorManager = InterceptorManager.getInstance();
-	}
-
-	private InterceptorManager interceptorManager;
-	private XMPPServer mServer;
-	private PresenceManager mPresenceManager;
-	private UserManager mUserManager;
-	private Gson mGson;
-
-	public void initializePlugin(PluginManager manager, File pluginDirectory) {
-		Log.info("GCM Plugin started");
-		
-		initConf();
-		mServer = XMPPServer.getInstance();
-		mPresenceManager = mServer.getPresenceManager();
-		mUserManager = mServer.getUserManager();
-		mGson = new Gson();
-		
-		interceptorManager.addInterceptor(this);
-	}
-
-	public void destroyPlugin() {
-		Log.info("GCM Plugin destroyed");
-		interceptorManager.removeInterceptor(this);
-	}
-
-	public void interceptPacket(Packet packet, Session session,
-			boolean incoming, boolean processed) throws PacketRejectedException {
-
-		if (processed) {
-			return;
-		}
-		if (!incoming) {
-			return;
-		}
-
-		if (packet instanceof Message) {
-			Message msg = (Message) packet;
-			process(msg);
-		}
-
-	}
-
-	private void process(final Message msg) {
-		if(mDebug)Log.info("GCM Plugin process() called");
-		try {
-			if (checkTarget(msg)) {
-				if(mDebug)Log.info("GCM Plugin Check=true");
-				TimerTask messageTask = new TimerTask() {
-					@Override
-					public void run() {
-							sendExternalMsg(msg);
-					}
-				};
-				TaskEngine.getInstance().schedule(messageTask, 20);
-			} else {
-				if(mDebug)Log.info("GCM Plugin Check=false");
-			}
-		} catch (UserNotFoundException e) {
-			Log.error("GCM Plugin (UserNotFoundException) Something went reeeaaaaally wrong");
-			e.printStackTrace();
-			// Something went reeeaaaaally wrong if you end up here!!
-		}
-	}
-
-	private boolean checkTarget(Message msg) throws UserNotFoundException {
-		if(msg.getBody() == null || msg.getBody().equals("")){
-			return false;
-		}
-		
-		JID toJID = msg.getTo().asBareJID();
-		if(mDebug)Log.info("GCM Plugin check() called");
-
-		if(!toJID.getDomain().contains(mServer.getServerInfo().getXMPPDomain())){
-			return false;
-		}
-		
-		if (mMode.equalsIgnoreCase(GcmPlugin.MODE_ALL)) {
-			return true;
-		} else if (mMode.equalsIgnoreCase(GcmPlugin.MODE_OFFLINE)) {
-			
-			String y = UserNameManager.getUserName(toJID);
-			if(mDebug)Log.info("GCM Plugin getUserName(...) = " + y);
-			User x = mUserManager.getUser(y);
-			if(mDebug)Log.info("GCM Plugin getUser(...) = " + x.toString());
-			try{
-				Presence z = mPresenceManager.getPresence(x);
-				if(z == null) return true;
-				if(mDebug)Log.info("GCM Plugin getPresence(...) = " + z.toString());
-				return !z.isAvailable();
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-		} else if (mMode.equalsIgnoreCase(GcmPlugin.MODE_NO_MOBILE)) {
-			
-		} else if (mMode.equalsIgnoreCase(GcmPlugin.MODE_EXCEPTION)) {
-
-		}
-
-		return true;
-	}
-	
-	private void sendExternalMsg(Message msg) {
-		if(mDebug)Log.info("GCM Plugin sendExternalMsg() called");
-
-		if(mURL == null){
-			Log.error("GCM Plugin: URL is null");
-			return;
-		}
-		
-		EventObject temp = new EventObject();
-		temp.setBody(msg.getBody());
-		temp.setTo(msg.getTo().toBareJID());
-		temp.setFrom(msg.getFrom().toBareJID());
+    public static final String DEFAULT_GCM_HOST = "gcm.googleapis.com";
+    public static final int DEFAULT_GCM_PORT = 5235;
+    public static final String GCM_ELEMENT_NAME = "gcm";
+    public static final String GCM_NAMESPACE = "google:mobile:data";
 
 
-		try {
-			if(mDebug){
-				String x = Request
-				.Post(mURL)
-//				.bodyForm(Form.form().add("data", mGson.toJson(temp)).build())
-				.bodyString(mGson.toJson(temp), ContentType.APPLICATION_JSON)
-				.execute()
-				.returnContent().asString();
-				Log.info("GCM Plugin sendMsg(): "+x);
-			} else {
-				Request
-				.Post(mURL)
-//				.bodyForm(Form.form().add("data", mGson.toJson(temp)).build())
-				.bodyString(mGson.toJson(temp), ContentType.APPLICATION_JSON)
-				.execute();
-			}
-		} catch (ClientProtocolException e) {
-			Log.error("GCM Plugin: ClientProtocolException");
-			Log.error("GCM Plugin: " + e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
-			Log.error("GCM Plugin: IOException");
-			Log.error("GCM Plugin: " + e.getMessage());
-			e.printStackTrace();
-		} catch (Exception e){
-			Log.error("GCM Plugin: (Unknown)Exception");
-			Log.error("GCM Plugin: " + e.getMessage());
-			e.printStackTrace();
-		}
-	}
+    private static final String MODE = "plugin.gcmh.mode";
+    private static final String DEBUG = "plugin.gcmh.debug";
+
+    public static final String MODE_ALL = "1";
+    public static final String MODE_OFFLINE = "2";
+    public static final String MODE_NO_MOBILE = "3";
+    public static final String MODE_EXCEPTION = "4";
+
+    public static final String DEBUG_ON = "1";
+    public static final String DEBUG_OFF = "2";
+
+    private CcsClient ccsClient;
+
+    private String mGCMHost;
+    private int mGCMPort;
+    private String mGCMApiKey;
+    private String mGCMSengerId;
+
+    private InterceptorManager interceptorManager;
+    private XMPPServer mServer;
+    private PresenceManager mPresenceManager;
+    private UserManager mUserManager;
+    private Gson mGson;
+
+    private String mMode;
+    private boolean mDebug = false;
+
+    public GcmPlugin() {
+        interceptorManager = InterceptorManager.getInstance();
+    }
 
 
-	String mURL, mMode;
-	boolean mDebug = false;
-	
-	private void initConf() {
-		mURL = this.getURL();
-		mMode = this.getMode();
-		if (this.getDebug()) {
-			mDebug = true;
-		} else {
-			mDebug = false;
-		}
-	}
+    private void initConf() {
+        mGCMHost = this.getHost();
+        mGCMPort = this.getPort();
+        mGCMApiKey = this.getApiKey();
+        mGCMSengerId = this.getSenderId();
+        mMode = this.getMode();
+        if (this.getDebug()) {
+            mDebug = true;
+        } else {
+            mDebug = false;
+        }
+    }
 
-	public void setURL(String pURL) {
-		JiveGlobals.setProperty(URL, pURL);
-		initConf();
-	}
+    public void initializePlugin(PluginManager manager, File pluginDirectory) {
+        Log.info("GCM Plugin started");
+        initConf();
+        mServer = XMPPServer.getInstance();
+        mPresenceManager = mServer.getPresenceManager();
+        mUserManager = mServer.getUserManager();
+        mGson = new Gson();
 
-	public String getURL() {
-		return JiveGlobals.getProperty(URL, null);
-	}
+        interceptorManager.addInterceptor(this);
+        ccsClient = new CcsClient(mGCMHost, mGCMPort, mGCMApiKey, mGCMSengerId);
+        try {
+            ccsClient.connect();
+        } catch (XMPPException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public String getMode() {
-		return JiveGlobals.getProperty(MODE, GcmPlugin.MODE_ALL);
-	}
+    public void destroyPlugin() {
+        Log.info("GCM Plugin destroyed");
+        interceptorManager.removeInterceptor(this);
+        ccsClient.disconnect();
+    }
 
-	public void setMode(String mode) {
-		JiveGlobals.setProperty(MODE, mode);
-		initConf();
-	}
+    public void interceptPacket(Packet packet, Session session,
+                                boolean incoming, boolean processed) throws PacketRejectedException {
 
-	public void setDebug(boolean mode) {
-		if(mode){
-			JiveGlobals.setProperty(DEBUG, GcmPlugin.DEBUG_ON);
-		} else {
-			JiveGlobals.setProperty(DEBUG, GcmPlugin.DEBUG_OFF);
-		}
-		initConf();
-	}
+        if (processed) {
+            return;
+        }
+        if (!incoming) {
+            return;
+        }
 
-	public boolean getDebug() {
-		if(JiveGlobals.getProperty(DEBUG, GcmPlugin.DEBUG_OFF).equalsIgnoreCase(GcmPlugin.DEBUG_ON)){
-			return true;
-		} else {
-			return false;
-		}
-	}
+        if (packet instanceof Message) {
+            Message msg = (Message) packet;
+            process(msg);
+        }
 
-	
+    }
 
+    private void process(final Message msg) {
+        if (mDebug) Log.info("GCM Plugin process() called");
+        try {
+            if (checkTarget(msg)) {
+                if (mDebug) Log.info("GCM Plugin Check=true");
+                TimerTask messageTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        sendExternalMsg(msg);
+                    }
+                };
+                TaskEngine.getInstance().schedule(messageTask, 20);
+            } else {
+                if (mDebug) Log.info("GCM Plugin Check=false");
+            }
+        } catch (UserNotFoundException e) {
+            Log.error("GCM Plugin (UserNotFoundException) Something went reeeaaaaally wrong");
+            e.printStackTrace();
+            // Something went reeeaaaaally wrong if you end up here!!
+        }
+    }
+
+    private boolean checkTarget(Message msg) throws UserNotFoundException {
+        if (msg.getBody() == null || msg.getBody().equals("")) {
+            return false;
+        }
+
+        JID toJID = msg.getTo().asBareJID();
+        if (mDebug) Log.info("GCM Plugin check() called");
+
+        if (!toJID.getDomain().contains(mServer.getServerInfo().getXMPPDomain())) {
+            return false;
+        }
+
+        if (mMode.equalsIgnoreCase(GcmPlugin.MODE_ALL)) {
+            return true;
+        } else if (mMode.equalsIgnoreCase(GcmPlugin.MODE_OFFLINE)) {
+
+            String y = UserNameManager.getUserName(toJID);
+            if (mDebug) Log.info("GCM Plugin getUserName(...) = " + y);
+            User x = mUserManager.getUser(y);
+            if (mDebug) Log.info("GCM Plugin getUser(...) = " + x.toString());
+            try {
+                Presence z = mPresenceManager.getPresence(x);
+                if (z == null) return true;
+                if (mDebug) Log.info("GCM Plugin getPresence(...) = " + z.toString());
+                return !z.isAvailable();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else if (mMode.equalsIgnoreCase(GcmPlugin.MODE_NO_MOBILE)) {
+
+        } else if (mMode.equalsIgnoreCase(GcmPlugin.MODE_EXCEPTION)) {
+
+        }
+
+        return true;
+    }
+
+    private void sendExternalMsg(Message msg) {
+        if (mDebug) Log.info("GCM Plugin sendExternalMsg() called");
+
+        if (!ccsClient.isConnected()) {
+            Log.error("GCM Plugin: Not connected to GCM");
+            return;
+        }
+
+        EventObject temp = new EventObject();
+        temp.setBody(msg.getBody());
+        temp.setTo(msg.getTo().toBareJID());
+        temp.setFrom(msg.getFrom().toBareJID());
+
+
+        try {
+            // Send a sample hello downstream message to a device.
+            Map<String, String> payload = new HashMap<String, String>();
+            payload.put("message", msg.getBody());
+            String collapseKey = "sample";
+            Long timeToLive = 10000L;
+            Boolean delayWhileIdle = true;
+            ccsClient.send(ccsClient.createJsonMessage("/topics/global", msg.getID(), payload, collapseKey,
+                    timeToLive, delayWhileIdle));
+        } catch (Exception e) {
+            Log.error("GCM Plugin: (Unknown)Exception");
+            Log.error("GCM Plugin: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    public String getHost() {
+        return JiveGlobals.getProperty(GCM_HOST, DEFAULT_GCM_HOST);
+    }
+
+    public void setHost(String gcmHost) {
+        JiveGlobals.setProperty(GCM_HOST, gcmHost);
+        initConf();
+    }
+
+    public int getPort() {
+        return JiveGlobals.getIntProperty(GCM_PORT, DEFAULT_GCM_PORT);
+    }
+
+    public void setPort(int gcmPort) {
+        JiveGlobals.setProperty(GCM_PORT, String.valueOf(gcmPort));
+        initConf();
+    }
+
+    public String getSenderId() {
+        return JiveGlobals.getProperty(GCM_SENDER_ID, null);
+    }
+
+    public void setSenderId(String gcmSenderId) {
+        JiveGlobals.setProperty(GCM_SENDER_ID, gcmSenderId);
+        initConf();
+    }
+
+    public String getApiKey() {
+        return JiveGlobals.getProperty(GCM_API_KEY, null);
+    }
+
+    public void setApiKey(String gcmApiKey) {
+        JiveGlobals.setProperty(GCM_API_KEY, gcmApiKey);
+        initConf();
+    }
+
+    public String getMode() {
+        return JiveGlobals.getProperty(MODE, GcmPlugin.MODE_ALL);
+    }
+
+    public void setMode(String mode) {
+        JiveGlobals.setProperty(MODE, mode);
+        initConf();
+    }
+
+    public boolean getDebug() {
+        if (JiveGlobals.getProperty(DEBUG, GcmPlugin.DEBUG_OFF).equalsIgnoreCase(GcmPlugin.DEBUG_ON)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setDebug(boolean mode) {
+        if (mode) {
+            JiveGlobals.setProperty(DEBUG, GcmPlugin.DEBUG_ON);
+        } else {
+            JiveGlobals.setProperty(DEBUG, GcmPlugin.DEBUG_OFF);
+        }
+        initConf();
+    }
 }
--- src/web/gcmh-form.jsp
+++ src/web/gcmh-form.jsp
@@ -10,126 +10,132 @@
 <%@ taglib uri="http://java.sun.com/jstl/fmt_rt" prefix="fmt"%>
 
 <%
-	boolean save = request.getParameter("save") != null;	
-	boolean gcmhDebug = ParamUtils.getBooleanParameter(request, "gcmhDebug", false);
-	boolean gcmhMode = ParamUtils.getBooleanParameter(request, "gcmhMode", false);
-	String gcmhHost = ParamUtils.getParameter(request, "gcmhHost");
-	int gcmhPort = ParamUtils.getParameter(request, "gcmhPort");
-	String gcmhSenderId = ParamUtils.getParameter(request, "gcmhSenderId");
-	String gcmhApiKey = ParamUtils.getParameter(request, "gcmhApiKey");
+    boolean save = request.getParameter("save") != null;
+    boolean gcmhDebug = ParamUtils.getBooleanParameter(request, "gcmhDebug", false);
+    boolean gcmhMode = ParamUtils.getBooleanParameter(request, "gcmhMode", false);
+    String gcmhHost = ParamUtils.getParameter(request, "gcmhHost");
+    int gcmhPort = ParamUtils.getIntParameter(request, "gcmhPort", 0);
+    String gcmhSenderId = ParamUtils.getParameter(request, "gcmhSenderId");
+    String gcmhApiKey = ParamUtils.getParameter(request, "gcmhApiKey");
 
-	GcmPlugin plugin = (GcmPlugin) XMPPServer.getInstance().getPluginManager().getPlugin("gcmh");
+    GcmPlugin plugin = (GcmPlugin) XMPPServer.getInstance().getPluginManager().getPlugin("gcmh");
 
-	Map<String, String> errors = new HashMap<String, String>();	
-	if (save) {
-	/*
-	  if (gcmhURL == null || gcmhURL.trim().length() < 1) {
-	     errors.put("missinggcmhURL", "missinggcmhURL");
-	  }
-	  */
-              
-	  if (errors.size() == 0) {
-	     //plugin.setEnabled(gcmhDebug);
-	     plugin.setHost(gcmhHost);
-	     plugin.setPort(gcmhPort);
-	     plugin.setSenderId(gcmhSenderId);
-	     plugin.setApiKey(gcmhApiKey);
+    Map<String, String> errors = new HashMap<String, String>();
+    if (save) {
+      if (gcmhHost == null || gcmhHost.trim().length() == 0) {
+         errors.put("missinggcmhHost", "missinggcmhHost");
+      }
+      if (gcmhPort == 0) {
+         errors.put("missinggcmhPort", "missinggcmhPort");
+      }
+      if (gcmhSenderId == null || gcmhSenderId.trim().length() == 0) {
+         errors.put("missinggcmhSenderId", "missinggcmhSenderId");
+      }
+      if (gcmhApiKey == null || gcmhApiKey.trim().length() == 0) {
+         errors.put("missinggcmhApiKey", "missinggcmhApiKey");
+      }
+
+      if (errors.size() == 0) {
+         //plugin.setEnabled(gcmhDebug);
+         plugin.setHost(gcmhHost);
+         plugin.setPort(gcmhPort);
+         plugin.setSenderId(gcmhSenderId);
+         plugin.setApiKey(gcmhApiKey);
          plugin.setDebug(gcmhDebug);
          if(gcmhMode){
-        	 plugin.setMode(GcmPlugin.MODE_OFFLINE);
+             plugin.setMode(GcmPlugin.MODE_OFFLINE);
          } else {
-        	 plugin.setMode(GcmPlugin.MODE_ALL);
+             plugin.setMode(GcmPlugin.MODE_ALL);
          }
-	     
-	     response.sendRedirect("gcmh-form.jsp?settingsSaved=true");
-	     return;
-	  }		
-	}
-    
-	//gcmhDebug = plugin.isEnabled();
-	gcmhHost = plugin.getHost();
-	gcmhPort = plugin.getPort();
-	gcmhSenderId = plugin.getSenderId();
-	gcmhApiKey = plugin.getApiKey();
-	gcmhDebug = plugin.getDebug();
-	if(plugin.getMode().equals(GcmPlugin.MODE_ALL)){
-		gcmhMode = false;
-	} else {
-		gcmhMode = true;
-	}
+
+         response.sendRedirect("gcmh-form.jsp?settingsSaved=true");
+         return;
+      }
+    }
+
+    //gcmhDebug = plugin.isEnabled();
+    gcmhHost = plugin.getHost();
+    gcmhPort = plugin.getPort();
+    gcmhSenderId = plugin.getSenderId();
+    gcmhApiKey = plugin.getApiKey();
+    if(plugin.getMode().equals(GcmPlugin.MODE_ALL)){
+        gcmhMode = false;
+    } else {
+        gcmhMode = true;
+    }
 
 %>
 
 <html>
-	<head>
-	  <title><fmt:message key="gcmh.title" /></title>
-	  <meta name="pageID" content="gcmh-form"/>
-	</head>
-	<body>
+    <head>
+      <title><fmt:message key="gcmh.title" /></title>
+      <meta name="pageID" content="gcmh-form"/>
+    </head>
+    <body>
 
 <form action="gcmh-form.jsp?save" method="post">
 
 <div class="jive-contentBoxHeader"><fmt:message key="gcmh.options" /></div>
 <div class="jive-contentBox">
-   
-	<% if (ParamUtils.getBooleanParameter(request, "settingsSaved")) { %>
-   
-	<div class="jive-success">
-	<table cellpadding="0" cellspacing="0" border="0">
-	<tbody>
-	  <tr>
-	     <td class="jive-icon"><img src="images/success-16x16.gif" width="16" height="16" border="0"></td>
-	     <td class="jive-icon-label"><fmt:message key="gcmh.saved.success" /></td>
-	  </tr>
-	</tbody>
-	</table>
-	</div>
-   
-	<% } %>
-   	
+
+    <% if (ParamUtils.getBooleanParameter(request, "settingsSaved")) { %>
+
+    <div class="jive-success">
+    <table cellpadding="0" cellspacing="0" border="0">
+    <tbody>
+      <tr>
+         <td class="jive-icon"><img src="images/success-16x16.gif" width="16" height="16" border="0"></td>
+         <td class="jive-icon-label"><fmt:message key="gcmh.saved.success" /></td>
+      </tr>
+    </tbody>
+    </table>
+    </div>
+
+    <% } %>
+
    <br>
-	<p><fmt:message key="gcmh.directions" /></p>
-   
-	<table cellpadding="3" cellspacing="0" border="0" width="100%">
-	<tbody>
-	  <tr>
-	     <td width="5%" valign="top"><fmt:message key="gcmh.host" />:&nbsp;</td>
-	     <td width="95%"><input type="text" name="gcmhHost" value="<%= gcmhHost %>"></td>
-	     <% if (errors.containsKey("missinggcmhHost")) { %>
-	        <span class="jive-error-text"><fmt:message key="gcmh.host.missing" /></span>
-	     <% } %> 
-	  </tr>
-	  <tr>
-	     <td width="5%" valign="top"><fmt:message key="gcmh.port" />:&nbsp;</td>
-	     <td width="95%"><input type="text" name="gcmhPort" value="<%= gcmhPort %>"></td>
-	     <% if (errors.containsKey("missinggcmhPort")) { %>
-	        <span class="jive-error-text"><fmt:message key="gcmh.port.missing" /></span>
-	     <% } %>
-	  </tr>
-	  <tr>
-	     <td width="5%" valign="top"><fmt:message key="gcmh.sender.id" />:&nbsp;</td>
-	     <td width="95%"><input type="text" name="gcmhSenderId" value="<%= gcmhSenderId %>"></td>
-	     <% if (errors.containsKey("missinggcmhSenderId")) { %>
-	        <span class="jive-error-text"><fmt:message key="gcmh.sender.id.missing" /></span>
-	     <% } %>
-	  </tr>
-	  <tr>
-	     <td width="5%" valign="top"><fmt:message key="gcmh.api.key" />:&nbsp;</td>
-	     <td width="95%"><input type="text" name="gcmhApiKey" value="<%= gcmhApiKey %>"></td>
-	     <% if (errors.containsKey("missinggcmhApiKey")) { %>
-	        <span class="jive-error-text"><fmt:message key="gcmh.api.key.missing" /></span>
-	     <% } %>
-	  </tr>
-	  <tr>
-	     <td width="1%" align="center" nowrap><input type="checkbox" name="gcmhMode" <%=gcmhMode ? "checked" : "" %>></td>
-	     <td width="99%" align="left"><fmt:message key="gcmh.mode" /></td>
-	  </tr>
-	  <tr>
-	     <td width="1%" align="center" nowrap><input type="checkbox" name="gcmhDebug" <%=gcmhDebug ? "checked" : "" %>></td>
-	     <td width="99%" align="left"><fmt:message key="gcmh.enable" /></td>
-	  </tr>
-	</tbody>
-	</table>
+    <p><fmt:message key="gcmh.directions" /></p>
+
+    <table cellpadding="3" cellspacing="0" border="0" width="100%">
+    <tbody>
+      <tr>
+         <td width="5%" valign="top"><fmt:message key="gcmh.host" />:&nbsp;</td>
+         <td width="95%"><input type="text" name="gcmhHost" value="<%= gcmhHost %>"></td>
+         <% if (errors.containsKey("missinggcmhHost")) { %>
+            <span class="jive-error-text"><fmt:message key="gcmh.host.missing" /></span>
+         <% } %>
+      </tr>
+      <tr>
+         <td width="5%" valign="top"><fmt:message key="gcmh.port" />:&nbsp;</td>
+         <td width="95%"><input type="text" name="gcmhPort" value="<%= gcmhPort %>"></td>
+         <% if (errors.containsKey("missinggcmhPort")) { %>
+            <span class="jive-error-text"><fmt:message key="gcmh.port.missing" /></span>
+         <% } %>
+      </tr>
+      <tr>
+         <td width="5%" valign="top"><fmt:message key="gcmh.sender.id" />:&nbsp;</td>
+         <td width="95%"><input type="text" name="gcmhSenderId" value="<%= gcmhSenderId %>"></td>
+         <% if (errors.containsKey("missinggcmhSenderId")) { %>
+            <span class="jive-error-text"><fmt:message key="gcmh.sender.id.missing" /></span>
+         <% } %>
+      </tr>
+      <tr>
+         <td width="5%" valign="top"><fmt:message key="gcmh.api.key" />:&nbsp;</td>
+         <td width="95%"><input type="text" name="gcmhApiKey" value="<%= gcmhApiKey %>"></td>
+         <% if (errors.containsKey("missinggcmhApiKey")) { %>
+            <span class="jive-error-text"><fmt:message key="gcmh.api.key.missing" /></span>
+         <% } %>
+      </tr>
+      <tr>
+         <td width="1%" align="center" nowrap><input type="checkbox" name="gcmhMode" <%=gcmhMode ? "checked" : "" %>></td>
+         <td width="99%" align="left"><fmt:message key="gcmh.mode" /></td>
+      </tr>
+      <tr>
+         <td width="1%" align="center" nowrap><input type="checkbox" name="gcmhDebug" <%=gcmhDebug ? "checked" : "" %>></td>
+         <td width="99%" align="left"><fmt:message key="gcmh.enable" /></td>
+      </tr>
+    </tbody>
+    </table>
 </div>
 <input type="submit" value="<fmt:message key="gcmh.button.save" />"/>
 </form>
